011848ab584d6b87f40dce40c32a8665509bd8c4
Amar Kachari <amarkachari@BZLAP068.local>
2019-07-29 11:54:14 +0530

    Store data into local storage | Fetch ALready Saved data from local storage

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-function fa() {
-    console.log('a lal');
+function initialLoad() {
+    let reminders = window.localStorage.getItem('reminders');
     
+    if (reminders && reminders!="null") {
+        reminders = JSON.parse(reminders);
+    } else {
+        reminders = [];
+    }
+
+    document.querySelector('#saveItemViewer').innerHTML='';
+    const node = document.querySelector('#saveItemViewer');
+    reminders.forEach(x => {
+        var aDiv = document.createElement("div");
+        aDiv.innerText = x.text;         // Create a text node
+        node.appendChild(aDiv);
+    });
     
 };
 function saveReminder() {
@@ -13,9 +26,17 @@ function saveReminder() {
         reminders = [];
         reminders.push({ text: v, id: Math.random() });
     }
+    document.querySelector('#saveItemViewer').innerHTML='';
+    const node = document.querySelector('#saveItemViewer');
+    reminders.forEach(x => {
+        var aDiv = document.createElement("div");
+        aDiv.innerText = x.text;         // Create a text node
+        node.appendChild(aDiv);
+    });
     window.localStorage.setItem('reminders', JSON.stringify(reminders));
     console.log('Working');
 }
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#saveItemBtn').addEventListener('click', saveReminder);
+    initialLoad()
 });
