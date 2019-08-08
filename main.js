616d8c9c5e17256abbdd170d197a2747de0511ee
Amar Kachari <amarkachari@BZ01603.local>
2019-08-08 13:01:55 +0530

    Remove Button

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 function initialLoad() {
     let reminders = window.localStorage.getItem('reminders');
-    
+
     if (reminders && reminders!="null") {
         reminders = JSON.parse(reminders);
     } else {
         reminders = [];
     }
-
-    document.querySelector('#saveItemViewer').innerHTML='';
-    const node = document.querySelector('#saveItemViewer');
-    reminders.forEach(x => {
-        var aDiv = document.createElement("div");
-        aDiv.innerText = x.text;         // Create a text node
-        node.appendChild(aDiv);
-    });
-    
+    reRender(reminders)
 };
+function reRender(reminders) {
+  document.querySelector('#saveItemViewer').innerHTML='';
+  const node = document.querySelector('#saveItemViewer');
+  reminders.forEach((x, i) => {
+      var aDiv = document.createElement("div");
+      var aCloseSpan = document.createElement("span");
+      aCloseSpan.innerText='x';
+      aCloseSpan.dataset.idx=i;
+      aDiv.innerText = x.text;        // Create a text node
+      aDiv.appendChild(aCloseSpan);
+      node.appendChild(aDiv);
+  });
+}
 function saveReminder() {
     let reminders = window.localStorage.getItem('reminders');
     var v = document.querySelector('#reminder-input').value;
@@ -26,17 +31,28 @@ function saveReminder() {
         reminders = [];
         reminders.push({ text: v, id: Math.random() });
     }
-    document.querySelector('#saveItemViewer').innerHTML='';
-    const node = document.querySelector('#saveItemViewer');
-    reminders.forEach(x => {
-        var aDiv = document.createElement("div");
-        aDiv.innerText = x.text;         // Create a text node
-        node.appendChild(aDiv);
-    });
     window.localStorage.setItem('reminders', JSON.stringify(reminders));
-    console.log('Working');
+    reRender(reminders);
+    document.querySelector('#reminder-input').value='';
+}
+function deleteReminderPoint(e) {
+  const target = e.target;
+  const deleteIdx = target.dataset.idx;
+  // ===========================================
+  let reminders = window.localStorage.getItem('reminders');
+  if (reminders && reminders!="null") {
+      reminders = JSON.parse(reminders);
+  } else {
+      reminders = [];
+  }
+  //////////////////////////
+  reminders.splice(deleteIdx, 1);
+  window.localStorage.setItem('reminders', JSON.stringify(reminders));
+  initialLoad();
 }
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#saveItemBtn').addEventListener('click', saveReminder);
     initialLoad()
+    document.querySelector('#saveItemViewer').addEventListener('click', deleteReminderPoint);
+
 });
